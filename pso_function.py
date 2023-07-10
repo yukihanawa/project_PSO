d69036422f383c6c71963fbfeeac3d5218f37643
yukihanawa <hanawayuki@hanawayukinoMacBook-Air.local>
2023-07-11 04:17:53 +0900

    wの値を動的に変化

--- pso_function.py
+++ pso_function.py
@@ -7,7 +7,7 @@ import merge_csv
 import itertools
 
 # パラメータ
-N = 100                 # 粒子数
+N = 200                 # 粒子数
 MAX_ITERATION = 1000  # 世代数
 D = 10                 # 次元数
 # w = 1                # 慣性係数
@@ -21,7 +21,7 @@ function_set = ["rastrigin", "rosenbrock"]
 # c2_set = [1.5, 2]
 
 # parameter_set = [(0.5, 2, 2), (0.5, 1.5, 2), (0.5, 1, 1.5)]
-parameter_set = [(0.8,2,2),(0.9,2,2)]
+parameter_set = [(0.8,2,2)]
 
 
 # 粒子クラス
